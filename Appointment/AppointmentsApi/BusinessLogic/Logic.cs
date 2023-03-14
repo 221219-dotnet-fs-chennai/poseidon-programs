d027d1e507f10c23aa502b960811e802f3004e82
MadhuVasanth230 <112465268+MadhuVasanth230@users.noreply.github.com>
2023-03-14 18:50:46 +0530

    appointmentApi added

---
+++ Appointment/AppointmentsApi/BusinessLogic/Logic.cs
@@ -0,0 +1,55 @@
+﻿using FluentApi;
+using FluentApi.Entities;
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using fe = FluentApi.Entities;
+namespace BusinessLogic
+{
+    public class Logic : ILogic
+    {
+         IRepo<fe.Appointment> aprepo;
+        public Logic (fe.AppointmentDbContext context)
+        {
+            aprepo = new AppointmentRepo(context);
+           
+        }
+        public fe.Appointment AddAppointment(Models.Appointment ap)
+        {
+            return aprepo.Add(Mapper.Map( ap));
+        }
+        public IEnumerable<Models.Appointment> GetAppointment()
+        {
+            return Mapper.Map(aprepo.GetAll());
+        }
+        public IEnumerable<Models.Appointment> GetAppointmentByAcceptance(int acceptVal)
+        {
+
+            return Mapper.Map(aprepo.GetByAcceptance(acceptVal));
+        }
+        public fe.Appointment UpdateAppointment(int PatientId, Models.Appointment ap)
+        {
+           
+            var apmt = (from apt in aprepo.GetAll()
+                           where apt.PatientId == PatientId
+                           select apt).FirstOrDefault();
+           
+            if (apmt != null)
+            {
+
+                apmt.Acceptance = ap.Acceptance;
+                apmt.PatientId= ap.PatientId;
+                apmt.Date = ap.Date;
+                apmt.SubmissionDate=ap.SubmissionDate;
+                apmt.Reason = ap.Reason;
+                apmt.PhysicianEmail = ap.PhysicianEmail;
+                apmt = aprepo.Update(apmt);
+
+
+            }
+
+            return apmt;
+        }
+
+    }
+}
