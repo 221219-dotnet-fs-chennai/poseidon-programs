fc9e16eb497656e135d6d2c2c418a19f2f2ff865
themonk-e <codenmad@gmail.com>
2023-03-17 15:11:30 +0530

    patient appointments page

---
+++ poseidon-healthcare/src/app/Patient/Book_Appointment/pat-appointment-content/pat-appointment-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pat-appointment-content',
+  templateUrl: './pat-appointment-content.component.html',
+  styleUrls: ['./pat-appointment-content.component.css']
+})
+export class PatAppointmentContentComponent {
+
+  
+}
