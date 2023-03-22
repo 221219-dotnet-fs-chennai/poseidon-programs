610458e752578f602aeef6cb79ebdbdc267a0ff2
Manikandan R <rmanikandan102000@gmail.com>
2023-03-22 16:13:27 +0530

    date format changed

--- poseidon-healthcare/src/app/Patient/Book_Appointment/pat-appointment-content/pat-appointment-content.component.ts
+++ poseidon-healthcare/src/app/Patient/Book_Appointment/pat-appointment-content/pat-appointment-content.component.ts
@@ -2,12 +2,46 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import {MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import { PatReasonDialogComponent } from '../pat-reason-dialog/pat-reason-dialog.component';
+
+import {FormControl} from '@angular/forms';
+import {MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+
+import * as _moment from 'moment';
+import { default as _rollupMoment, Moment } from 'moment';
+
+const moment = _rollupMoment || _moment;
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
 @Component({
   selector: 'app-pat-appointment-content',
   templateUrl: './pat-appointment-content.component.html',
-  styleUrls: ['./pat-appointment-content.component.css']
+  styleUrls: ['./pat-appointment-content.component.css'],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
 })
 export class PatAppointmentContentComponent {
+
+  date = new FormControl(moment());
+
   constructor(private router: Router, public dialog: MatDialog) {
 
   }
--- poseidon-healthcare/src/app/services/admin/admin-add-doctor/admin-add-doctor.component.ts
+++ poseidon-healthcare/src/app/services/admin/admin-add-doctor/admin-add-doctor.component.ts
@@ -2,28 +2,55 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+
+import * as _moment from 'moment';
+import { default as _rollupMoment, Moment } from 'moment';
+
+const moment = _rollupMoment || _moment;
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
 @Component({
   selector: 'app-admin-add-doctor',
   templateUrl: './admin-add-doctor.component.html',
-  styleUrls: ['./admin-add-doctor.component.css']
+  styleUrls: ['./admin-add-doctor.component.css'],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
 })
 export class AdminAddDoctorComponent {
 
-  constructor(private router: Router)
-  {
+  date = new FormControl(moment());
 
+  constructor(private router: Router) {
   }
+  
   onSubmit(f: NgForm) {
     console.log(f);
   }
 
   getValue(f: any) {
-
   }
 
-  to_admin()
-  {
+  to_admin() {
     this.router.navigate(['adminhome'])
   }
-  
 }
