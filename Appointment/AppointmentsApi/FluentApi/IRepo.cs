783fca9d633239d7921c0b4d52fed2b85120e222
ViswanathamMounika <mounikav.199@gmail.com>
2023-03-23 16:14:21 +0530

    appointment api  changes

--- Appointment/AppointmentsApi/FluentApi/IRepo.cs
+++ Appointment/AppointmentsApi/FluentApi/IRepo.cs
@@ -11,6 +11,8 @@ namespace FluentApi
         T Add(T t);
         List<T> GetAll();
         List<T> GetByAcceptance(int t);
+
+        List<T> GetByPatientId(int t);
         T Update(T t);
 
     }
